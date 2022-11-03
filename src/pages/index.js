61267f9d885a044dd2e1467aec68992a52271744
kassimali <kassimali114@gmail.com>
2022-11-03 10:24:42 +0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import AuthWrapper from "./AuthWrapper";
+import PrivateRoute from "./PrivateRoute";
+import Error from "./Error";
+
+// dev-0b3la3o4417oe0tt.us.auth0.com
+// qHs0RBhasP5Z8Yeq8SKMMSv6IDuPj6ro
+export { Dashboard, Login, AuthWrapper, PrivateRoute, Error };
